62728ea160c709e2ec5bb044397645a3e94bd1ff
Edgar Guerrero <edgargn87@gmail.com>
2024-08-09 18:58:31 -0500

    feat(warehouse): create the get one inventory by ingredient service

--- src/warehouse/domain/entities/inventory.entity.ts
+++ src/warehouse/domain/entities/inventory.entity.ts
@@ -2,6 +2,9 @@ export class InventoryEntity {
   public id?: number;
   public ingredient_id: number;
   public quantity: number;
-  public updated_at: Date;
-  public created_at: Date;
+  public updated_at?: Date;
+  public created_at?: Date;
+}
+export class GetOneInventoryByIngredientIdEntity {
+  public ingredient_id: number;
 }
--- src/warehouse/infrastructure/repositories/inventory.repository.impl.ts
+++ src/warehouse/infrastructure/repositories/inventory.repository.impl.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
-
-import { IInventoryRepository } from '@warehouse/domain/repositories/inventory.repository';
-import { DatabaseRepository } from './database.repository';
-import { Inventory } from '../entities/db/inventory.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+
+import { IInventoryRepository } from '@warehouse/domain/repositories/inventory.repository';
+
+import { DatabaseRepository } from './database.repository';
+
+import { Inventory } from '../entities/db/inventory.entity';
 
 @Injectable()
 export class InventoryRepositoryImpl
